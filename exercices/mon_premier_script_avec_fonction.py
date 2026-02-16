773b3d5c35865a127914d76a689c2952c078abf9
abots <bots.alexandre@gmail.com>
2026-02-16 15:52:16 +0000

    test: clean code de la fonction name + correction de setup tools

--- exercices/mon_premier_script_avec_fonction.py
+++ exercices/mon_premier_script_avec_fonction.py
@@ -1,22 +1,34 @@
 import unittest
+from typing import List
 
-"""
-Count names with more than seven letters
-"""
-def names(prenoms):
-    more_than_seven = 0
-    for prenom in prenoms:
-        if len(prenom) > 7:
-            more_than_seven += 1
-            print(prenom + " est un prénom avec un nombre de lettres supérieur à 7")
+MAX_NAME_LENGTH: int = 7
+
+
+def count_long_names(first_names: List[str]) -> int:
+    """
+    Count the number of first names longer than MAX_NAME_LENGTH
+    and display a message for each name.
+    """
+    long_name_count: int = 0
+
+    for first_name in first_names:
+        if is_name_long(first_name):
+            print(f"{first_name} contient plus de {MAX_NAME_LENGTH} lettres")
+            long_name_count += 1
         else:
-            print(prenom + " est un prénom avec un nombre de lettres inférieur ou égal à 7")
-    return more_than_seven
+            print(f"{first_name} contient {MAX_NAME_LENGTH} lettres ou moins")
+
+    return long_name_count
+
+
+def is_name_long(first_name: str) -> bool:
+    """Return True if the name length exceeds MAX_NAME_LENGTH."""
+    return len(first_name) > MAX_NAME_LENGTH
 
 class TestNamesMethod(unittest.TestCase):
      def test_names(self):
-        prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
-        more_than_seven = names(prenoms=prenoms)
+        first_names = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
+        more_than_seven = count_long_names(first_names=first_names)
         self.assertEqual(more_than_seven, 4)
 
 if __name__ == '__main__':
